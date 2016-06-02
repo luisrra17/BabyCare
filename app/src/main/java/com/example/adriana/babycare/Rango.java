fedc3d4f902334a3bcc972a8e7bd33ef3b3a868a
luisrra17 <luisrra17@gmail.com>
2016-06-01 18:03:26 -0600

    2.0

--- app/src/main/java/com/example/adriana/babycare/Rango.java
+++ app/src/main/java/com/example/adriana/babycare/Rango.java
@@ -1,76 +1,99 @@
 package com.example.adriana.babycare;
 
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+    import java.util.ArrayList;
+    import java.util.HashMap;
+    import java.util.List;
+    import android.app.Activity;
+    import android.os.Bundle;
+    import android.view.Menu;
+    import android.view.View;
+    import android.widget.AdapterView;
+    import android.widget.AdapterView.OnItemClickListener;
+    import android.widget.LinearLayout;
+    import android.widget.ListView;
+    import android.widget.SimpleAdapter;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+    public class Rango extends Activity {
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
+        // Array of strings storing country names
+        String[] countries = new String[]{
+                "3 - 6 MESES",
+                "6 - 9 MESES",
+                "9 - 12 MESES",
+                "12 - 18 MESES",
+                "18 - 24 MESES",
+                "24 - 36 MESES",
+                "36+ MESES",
+        };
 
-public class Rango extends Activity {
-
-    // Array of strings storing country names
-    String[] countries = new String[] {
-            "3 - 6 MESES",
-            "6 - 9 MESES",
-            "9 - 12 MESES",
-            "12 - 18 MESES",
-            "18 - 24 MESES",
-            "24 - 36 MESES",
-            "36+ MESES",
-    };
-
-    // Array of integers points to images stored in /res/drawable-ldpi/
-    int[] flags = new int[]{
-            R.drawable.b1,
-            R.drawable.b2,
-            R.drawable.b3,
-            R.drawable.b4,
-            R.drawable.b5,
-            R.drawable.b6,
-            R.drawable.b7,
-    };
+        // Array of integers points to images stored in /res/drawable-ldpi/
+        int[] flags = new int[]{
+                R.drawable.b1,
+                R.drawable.b2,
+                R.drawable.b3,
+                R.drawable.b4,
+                R.drawable.b5,
+                R.drawable.b6,
+                R.drawable.b7,
+        };
 
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rango);
+        /**
+         * Called when the activity is first created.
+         */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_rango);
 
-        // Each row in the list stores country name, currency and flag
-        List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
+            // Each row in the list stores country name, currency and flag
+            List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
 
-        for(int i=0;i<7;i++){
-            HashMap<String, String> hm = new HashMap<String,String>();
-            hm.put("txt", countries[i]);
-            hm.put("flag", Integer.toString(flags[i]) );
-            aList.add(hm);
+            for (int i = 0; i < 7; i++) {
+                HashMap<String, String> hm = new HashMap<String, String>();
+                hm.put("txt", countries[i]);
+                hm.put("cur", "");
+                hm.put("flag", Integer.toString(i+1));
+                aList.add(hm);
+            }
+
+            // Keys used in Hashmap
+            String[] from = {"flag", "txt", "cur"};
+
+            // Ids of views in listview_layout
+            int[] to = {R.id.flag, R.id.txt, R.id.cur};
+
+            // Instantiating an adapter to store each items
+            // R.layout.listview_layout defines the layout of each item
+            SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), aList, R.layout.listview_layout, from, to);
+
+            // Getting a reference to listview of main.xml layout file
+            ListView listView = (ListView) findViewById(R.id.listviews);
+
+            // Setting the adapter to the listView
+            listView.setAdapter(adapter);
+
+            // Item Click Listener for the listview
+            AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View container, int position, long id) {
+                    // Getting the Container Layout of the ListView
+                    LinearLayout linearLayoutParent = (LinearLayout) container;
+
+                    // Getting the inner Linear Layout
+                    LinearLayout linearLayoutChild = (LinearLayout) linearLayoutParent.getChildAt(1);
+
+                    // Getting the Country TextView
+                    TextView tvCountry = (TextView) linearLayoutChild.getChildAt(0);
+
+                    Toast.makeText(getBaseContext(), tvCountry.getText().toString(), Toast.LENGTH_SHORT).show();
+                }
+            };
+
+            // Setting the item click listener for the listview
+            listView.setOnItemClickListener(itemClickListener);
         }
 
-        // Keys used in Hashmap
-        String[] from = { "flag","txt","cur" };
-
-        // Ids of views in listview_layout
-        int[] to = { R.id.flag,R.id.txt,R.id.cur};
-
-        // Instantiating an adapter to store each items
-        // R.layout.listview_layout defines the layout of each item
-        SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), aList, R.layout.listview_layout, from, to);
-
-        // Getting a reference to listview of main.xml layout file
-        ListView listView = ( ListView ) findViewById(R.id.listview);
-
-
-        // Setting the adapter to the listView
-        listView.setAdapter(adapter);
-
     }
-}
